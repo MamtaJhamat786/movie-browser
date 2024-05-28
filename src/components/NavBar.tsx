afd803ea1894f678c52d84a0d010e0ae42541a25
Mamta <mamta@erply.com>
2024-05-28 21:08:56 +0300

    remove unnecessary stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { Box, AppBar, Typography, Badge } from "@mui/material";
 import { setMovieInfo } from "../store/active/reducer";
-import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
-import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
 import { useAppDispatch, useAppSelector } from "../store";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import MenuIcon from '@mui/icons-material/Menu';
-
 
 export const NavBar :React.FC =()=> {
     const item= useAppSelector((s)=> s.active.itemsInBag)
@@ -32,26 +28,9 @@ export const NavBar :React.FC =()=> {
                 <Link  to="/" style={{textDecoration: 'none'}} >
                     <Typography onClick={setMovieEmpty} fontWeight='700' sx={{color: 'red',  fontSize: '24px', fontFamily: 'Segoe UI'}}>TMDB</Typography>
                 </Link>
-                <Box gap='10px' display="flex">
-                    <Badge color="success" variant="dot"
-                           anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'right',
-                    }}>
-                        <PersonRoundedIcon sx={{color: 'black'}} />
-                    </Badge>
-                    {mobileView ? (
-                        <MenuIcon sx={{color: 'black'}} />
-                        ):(
-                        <>
-                            <Typography sx={{color: 'black'}}>Mamta</Typography>
-                            <FavoriteRoundedIcon sx={{color: 'red'}} />
-                            <Badge color="primary" badgeContent={item}>
-                                <AddShoppingCartRoundedIcon sx={{color: 'black'}}/>
-                            </Badge>
-                        </>
-                        )}
-                </Box>
+                <Badge color="primary" badgeContent={item}>
+                    <AddShoppingCartRoundedIcon sx={{color: 'black'}}/>
+                </Badge>
             </Box>
         </AppBar>
     )
